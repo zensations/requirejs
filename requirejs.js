b0c024b5ef63ae610f21b2d43d367126d141df33
zensations <pm@zensations.at>
2012-05-26 11:29:50 +0200

    Added jquery and drupal defines.

--- requirejs.js
+++ requirejs.js
@@ -1,8 +1,16 @@
-Drupal.behaviors.requirejs = {
-  attach: function (context, settings) {
-    require.config({
-      baseUrl: settings.requirejs.baseUrl,
-      paths: settings.requirejs.paths
-    });
-  }
-};
+(function($) {
+  Drupal.behaviors.requirejs = {
+    attach: function (context, settings) {
+      require.config({
+        baseUrl: settings.requirejs.baseUrl,
+        paths: settings.requirejs.paths
+      });
+      define('jquery', function() {
+        return $;
+      });
+      define('drupal', function() {
+        return Drupal;
+      });
+    }
+  };
+}(jQuery));
